2ae48e29934d6e12c12f33f32e3bdbb4775e6579
hhgzeng <hanhanguaizj@gmail.com>
2026-04-08 22:01:50 +0800

    feat: update version numbers for double-finger and one-tab scripts to 1.2 and 6.3 respectively

--- double-finger/double-finger.user.js
+++ double-finger/double-finger.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         恢复双指滑动导航
 // @namespace    https://github.com/hhgzeng
-// @version      1.1
+// @version      1.2
 // @description  解决部分网站因 CSS 拦截导致 Chrome 双指滑动导航失效的问题
 // @author       hhgzeng
 // @license      MIT
